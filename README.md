adc617545a67d8aada5e2df4439e4d87fb67795b
Alexander kruger <alex.bigbox@gmail.com>
2021-05-21 10:00:32 -0300

    Financeira em NodeJS Principais Funcões

---
+++ README.md
@@ -0,0 +1,27 @@
+## FinAPI - Financeira
+
+
+----
+
+
+### Requisitos
+
+- [] Deve ser possível criar uma conta
+- [] Deve ser possível buscar o extrato bancário do cliente
+- [] Deve ser possível realizar um depósito
+- [] Deve ser possível realizar um saque
+- [] Deve ser possível buscar o extrato por data
+- [] Deve ser possível atualizar dados da conta do cliente
+- [] Deve ser possível obter dados da conta do cliente
+- [] Deve ser possível deletar uma conta
+
+###  Regras de negócio
+
+- [] Não Deve ser possível cadastrar um conta com CPF já existente
+- [] Não Deve ser possível fazer depósito em um conta não existente
+- [] Não Deve ser possível buscar extrato em um conta não existente
+- [] Não Deve ser possível fazer saque em um conta não existente
+- [] Não Deve ser possível excluir uma conta não existente
+- [] Não Deve ser possível fazer saque quando o saldo for insuficiente
+
+
---
+++ src/index.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const { v4: uuidv4 } = require("uuid") 
+
+
+const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return response.status(400).json({ error: "Customer not found"});
+    }
+
+    request.customer = customer;
+    return next();
+}
+
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc + operation.amount;
+        } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+
+    return balance;
+}
+
+// Criar uma conta
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlreadyExists) {
+        return response.status(400).json({ error: "Customer already exists!"});
+    }
+
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    })
+
+    return response.status(201).send();
+});
+
+
+// app.use(verifyIfExistsAccountCPF);
+
+// Buscar o extrato
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+    return response.json(customer.statement);
+});
+
+
+// Fazer um depósito 
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
+
+// Fazer um saque
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+    
+
+    if(balance < amount) {
+        return response.status(400).json({error: "Insuficient founds!"});
+    }
+
+    
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+
+})
+
+app.listen(3333)
